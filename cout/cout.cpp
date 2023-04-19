03ad6927f72face8589fd1cb5261764c640d2f68
SunValley <SunValley@DESKTOP-BUQL0OM>
2023-04-19 12:20:27 +0300

    Добавьте файлы проекта.

---
+++ cout/cout.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\tApril 2023\n";
+    cout << "|Mon|Tue|Wed|Thu|Fri|Sat|Sun|\n";
+    cout << "-----------------------------\n";
+    cout << "|\t\t \t| 1 |\n";
+    cout << "-----------------------------\n";
+    cout << "| 2 | 3 | 4 | 5 | 6 | 7 | 8 |\n";
+    cout << "-----------------------------\n";
+    cout << "| 9 |10 |11 |12 |13 |14 |15 |\n";
+    cout << "-----------------------------\n";
+    cout << "|16 |17 |18 |19 |20 |21 |22 |\n";
+    cout << "-----------------------------\n";
+    cout << "|23 |24 |25 |26 |27 |28 |29 |\n";
+    cout << "-----------------------------\n";
+    cout << "|30 |\t \t \t    |\n";
+    cout << "-----------------------------\n";
+    cout << "\n\n\n";
+
+
+    cout << "      |\n";
+    cout << "     / \\ ___\n";
+    cout << "    /   \\| |\n";
+    cout << "   / ___ \\ |\n";
+    cout << "  /  [ ]  \\|\n";
+    cout << " /   ~~~   \\\n";
+    cout << "/           \\\n";
+    cout << "*************\n";
+    cout << "| ___   ___  |~~~~~~~~~~~~~~~~~~~~\n";
+    cout << "| | |   | |  |\t{}\t{}\t{}\n";
+    cout << "| | |   ~~~  |\t{}\t{}\t{}\n";
+    cout << "**********************************\n";
+    cout << "\n\n\n";
+
+
+    cout << "******** \t ***\t  ***\t  ***\t ***\t\t *****           \n";
+    cout << "***\t*** \t ***\t ****\t  ***\t***\t\t** ***            \n";
+    cout << "***\t ***\t ***\t*****\t  ***  ***\t       **  ***      \n";
+    cout << "***\t*** \t ***   ** ***\t  *** ***\t      **   ***     \n";
+    cout << "******** \t ***  **  ***\t  ******\t     *********      \n";
+    cout << "***\t*** \t *** **   ***\t  *** *** \t    **********    \n";
+    cout << "***\t ***\t *****\t  ***\t  ***  ***  \t   **      ***  \n";
+    cout << "***\t*** \t ****\t  ***\t  ***\t*** \t  **       ***    \n";
+    cout << "******** \t ***\t  ***\t  ***\t ***\t **        ***     \n";
+    cout << "\n\n\n";
+
+
+    cout << "         *  _                                     \n";
+    cout << "    _ -       - _                                 \n";
+    cout << "* -              * - _                            \n";
+    cout << "                        - _                       \n";
+    cout << "\t\t\t    *-----------------*                     \n";
+    cout << "\t\t\t      \\              /                     \n";
+    cout << "\t\t\t       \\            /                      \n";
+    cout << "\t\t\t        *----------*                        \n";
+    cout << "\n\n\n";
+
+
+    cout << "Summary\n";
+    cout << "----------------------------------------------------------------------\n";
+    cout << "Name:Buchko Victoria\n";
+    cout << "Age:17 years\n";
+    cout << "Odessa. Odessa region. Ukraine\n\n";
+    cout << "----------------------------------------------------------------------\n";
+    cout << "Contact details:\n\tbuckovika0@gmail.com\n\thttps://github.com/VictoriaBuchko(Github)\n\n";
+    cout << "General information:\n\t-student of the course Software Development at Step IT Academy University\n\t-1st year student of 121 specialty \"Software Engineering\" of the State University of Intellectual Technologies and Communication\n\n";
+    cout << "Work experience:\n\t-no work experience\n\n";
+    cout << "Goal for the future:\n\tWork as a senior backend developer in a large company\n";
+    cout << "----------------------------------------------------------------------\n";
+    return 0;
+}
+
